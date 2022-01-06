aaba137374374374bd6e394fce45697096433142
Ahmed <ahmed.kamal@namshi.com>
2022-01-06 18:22:53 +0400

    upgrade and simplify the api

---
+++ __tests__/index2Test.js
@@ -0,0 +1,145 @@
+const mysqlInitFn = require('../index');
+const {MySqlContainer} = require("testcontainers/dist/modules/mysql/mysql-container");
+
+jest.setTimeout(90000);
+
+describe('MySqlDriver', () => {
+    let container;
+    let mysql;
+
+
+    beforeAll(async () => {
+        container = await new MySqlContainer()
+            .withDatabase("test")
+            .start();
+
+        const config = {
+            host: container.getHost(),
+            port: container.getPort(),
+            database: container.getDatabase(),
+            user: container.getUsername(),
+            password: container.getUserPassword(),
+            waitForConnections: true,
+            connectionLimit: 5,
+            queueLimit: 1000,
+            connectTimeout: 10000,
+        }
+
+        mysql = await mysqlInitFn(config);
+    });
+
+    afterAll(async () => {
+        await mysql.stop();
+        if (container) {
+            await container.stop();
+        }
+    });
+
+    test("table created ", async () => {
+
+        await mysql.query(`
+
+            CREATE TABLE if not exists user
+            (
+                id   INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+                name VARCHAR(50)
+            );
+        `)
+
+        await mysql.query('truncate table user');
+        expect(await mysql.query(`SHOW TABLES LIKE 'user';`)).toHaveLength(1)
+    });
+
+    test("table is empty ", async () => {
+
+        let res = await mysql.query(`select count(1) as count
+                                     from user`);
+        expect(res[0]['count']).toStrictEqual(0)
+    });
+    test("COMMITTED transaction ", async () => {
+
+        //
+        await mysql.bulkInsert('insert into user (id, name) values ?', [[222, 'xxx_xxx']]);
+        let user = await mysql.query(`select id, name
+                                      from user
+                                      where id = ?`, [222]);
+
+        expect(user[0].name).toStrictEqual('xxx_xxx');
+
+
+        await mysql.transactional(async conn => {
+
+            await conn.query('UPDATE user SET name = ? where id = ?', ['TTTTTTT', '222']);
+            //add many update queries ...
+        });
+
+        let updatedUser = await mysql.query(`select id, name
+                                             from user
+                                             where id = ?`, [222]);
+
+        expect(updatedUser[0].name).toStrictEqual('TTTTTTT');
+    });
+
+    test("ROLLBACK transaction (data haven't changed)", async () => {
+
+        //
+        await mysql.bulkInsert('insert into user (id, name) values ?', [[333, 'xxx_xxx']]);
+        let user = await mysql.query('select id, name from user where id = ?', [333]);
+
+        expect(user[0].name).toStrictEqual('xxx_xxx');
+
+        // try {
+        //     ;
+        // } catch (e) {
+        //     //do nothing
+        // }
+        // bigger than name length in table
+        // let bigValue = "";
+        // for (let i = 0; i < 50; i++) {
+        //     bigValue += "TT";
+        // }
+
+        try {
+            await mysql.transactional(async conn => {
+                await conn.query('UPDATE user SET name = ? where id = ?', ["any val", '333']);
+                throw new Error("unexpected error!!");
+            })
+        } catch (e) {
+            // console.log("Data too long for column 'name' at row 1");
+            // handle the error
+        }
+
+        let updatedUser = await mysql.query('select id, name from user where id = ?', [333]);
+
+        expect(updatedUser[0].name).toStrictEqual('xxx_xxx');
+    });
+
+
+    test('insert bulk', async () => {
+        jest.setTimeout(60000);
+        let input = [];
+
+        for (let i = 0; i < 10; i++) {
+            input.push([i, `test ${i}`])
+        }
+
+        await mysql.bulkInsert('insert into user (id, name) VALUES ? ', input);
+    }, );
+
+    test(`select one row using the direct pool api
+            not recommended as you have to release the connection your selfe`, async () => {
+        let query = `SELECT name
+                     from user
+                     where id = ?`;
+        let params = [333]
+
+
+        const connection = await mysql.pool.getConnection();
+        let [res,] = await connection.execute(query, params);
+
+        await connection.release();
+
+        expect(res).toHaveLength(1)
+    });
+
+});
--- index.js
+++ index.js
@@ -1,252 +1,91 @@
 /* jshint esversion: 6 */
 const mysql2 = require('mysql2/promise');
+
 let instances = {};
 
-function DB() {
-  this.pool = null;
-  this.sessionConfig = null;
-  this.debug = false;
+const createPoolByConfig = async (config) => await mysql2.createPool(config);
+
+const query = (pool) => (query, params) => runQuery(pool, query, params);
+
+const bulkInsert = (pool) => async (query, params) => {
+    const connection = await pool.getConnection();
+    await connection.query(query, [params]);
+    await connection.release();
 }
 
-function setSessionConfig(connection, config) {
-  if(!config) {
-    return;
-  }
+const transactional = (pool) =>
+    async (func) => {
 
-  if(this.debug) {
-    console.log(`Set Session Config ${config}`);
-  }
+        const connection = await pool.getConnection();
+        await connection.query('START TRANSACTION');
 
-  return Promise.all(Object.entries(config).map(([key, value]) =>
-    connection.query(`SET SESSION ${key} = ?`, [value])
-  ));
-}
-
-function runQueryWith(query, params) {
-  let connection;
-
-  return this.getConnection().then((conn)=> {
-    connection = conn;
-    if (this.debug) {
-      console.log('getting connection');
-      console.info('QUERY AND PARAMS', query, params);
-    }
-
-    return connection.execute(query, params);
-  }).then((results) => {
-    connection && connection.connection && connection.connection.unprepare(query);
-    connection && connection.release();
-    if (this.debug) {
-      console.log('ok - released connection');
-    }
-
-    return results[0];
-  }).catch(err => {
-    if (!connection) {
-      console.error('could not establish db connection', err.message);
-      throw err;
-    }
-
-    connection.connection && connection.connection.unprepare(query);
-    connection.release();
-
-    throw err;
-  });
-}
-
-function flatten(params) {
-  return params.reduce((acc, param) =>
-    Array.isArray(param) ? [...acc, ...param] : [...acc, param],
-  []);
-}
-
-/**
- * If params contain arrays, the corresponding placeholders are expanded
- * with extra placeholders to match arrays' lengths and params are flattened.
- *
- * @param  {String} query
- * @param  {Array} params
- * @return {Array}
- */
-DB.prototype.prepareQuery = function(query, params) {
-  if(Array.isArray(params)){
-    let i = 0;
-    query = query.replace(/\?/g, function (match){
-      let param = params[i++];
-      return Array.isArray(param) ? Array(param.length).fill('?').join(',') : '?'
-    });
-    params = flatten(params);
-  }
-
-  return [query, params];
-}
-
-/**
- * Format a query as a preapred statement for bulk insert.
- *
- * @param  {String} query
- * @param  {Array} values
- * @return {Array}
- */
-DB.prototype.prepareBulk = function(query, values) {
-  if(!Array.isArray(values) || (values[0] && !Array.isArray(values[0]))) {
-    throw new Error(`Please provide an array of arrays for bulk insert like [[1,2], [1,3]]`);
-  }
-
-  let placeholders = '';
-
-  for (let i = 0; i < values.length; i++) {
-    placeholders += `(${Array(values[i].length).fill('?')})`;
-    placeholders += `${(values.length - 1  != i) ? ',' : ''}`;
-  }
-
-  return [
-    query.replace(`?`, placeholders),
-    values.reduce((acc, cur) => acc.concat(cur), [])
-  ];
-}
-
-/**
- * We are overiding the select method so that it returns the rows not the metadata
- * that is included in results[1]
- * @return {Object} Connection
- */
-DB.prototype.getConnection = function () {
-  return this.pool.getConnection().then((conn)=> {
-    if (this.debug) {
-      console.log('getting connection');
-    }
-
-    let exec = conn.execute;
-    conn.select = function(query, params) {
-      return exec.call(this, query, params).then((results) => {
-        return results[0];
-      });
+        try {
+            // run queries in transaction
+            await func(connection);
+            //then commit
+            await connection.query('COMMIT');
+        } catch (e) {
+            //ROLLBACK on error
+            await connection.query('ROLLBACK');
+            throw e;
+        } finally {
+            // release
+            await connection.release();
+        }
     };
 
-    if(this.sessionConfig) {
-      return setSessionConfig(conn, this.sessionConfig).then(() => conn)
+const runQuery = async (pool, query, params) => {
+    const connection = await pool.getConnection();
+    let [res,] = await connection.execute(query, params);
+    await connection.release();
+    return res;
+};
+
+const endPool = (dbName, pool) => async () => {
+    await pool.end();
+    delete instances[dbName];
+}
+
+const createNewDbConnection = async (name, config) => {
+
+    let instance = {};
+
+    // connection pool
+    instance.pool = await createPoolByConfig(config);
+
+    // function to stop/end pool connections
+    instance.stop = await endPool(name, instance.pool);
+
+    // async query runner function that return only result
+    instance.query = query(instance.pool);
+    instance.bulkInsert = bulkInsert(instance.pool);
+    instance.transactional = transactional(instance.pool);
+
+    return instance;
+}
+
+const init = async (opts) => {
+    if (!opts || Object.keys(opts).length <= 0) {
+        throw new Error('The config object cannot be empty');
     }
 
-    return conn;
-  });
-};
+    let config = JSON.parse(JSON.stringify(opts));
 
-/**
- * Setup the Database connection pool for this instance
- * @param  {Object} config
- */
-DB.prototype.configure = function (config) {
-  if ('debug' in config) {
-    this.debug = config.debug;
-    delete config.debug;
-  }
+    // db name for caching
+    let name
+    if (config.name) {
+        name = config.name;
+        delete config.name;
+    } else {
+        name = '_default_';
+    }
 
-  if ('sessionConfig' in config) {
-    this.sessionConfig = config.sessionConfig;
-    delete config.sessionConfig;
-  }
+    if (!instances[name]) {
+        instances[name] = await createNewDbConnection(name, config);
+    }
 
-  this.pool = mysql2.createPool(config);
-};
-
-/**
- * Run a DB query using prepared statements. This function does not support batch
- * operations.
- * @param  {String} query
- * @param  {Object} [params]
- * @return {Promise}
- */
-DB.prototype.query = function(query, params) {
-  if(Array.isArray(params)){
-    [query, params] = this.prepareQuery(query, params);
-  }
-
-  return runQueryWith.call(this, query, params);
+    return instances[name];
 }
 
 
-/**
- * Run a DB query using prepared statements. This function supports batch operations
- *
- * @param  {String} query
- * @param  {Object} [params]
- * @return {Promise}
- */
-DB.prototype.bulk = function(query, params) {
-  [query, params] = this.prepareBulk(query, params);
-  return runQueryWith.call(this, query, params);
-}
-
-/**
-* Set Session wait timeout parameter to close connection if inactive
-* Initiate transaction so all the subsequent queries to the db are not written
-* to the database until commit is called
-* @param  {String} timeout
-*
-* @return {object} connection
-**/
-DB.prototype.startTransaction = function (timeout) {
-  timeout = timeout || 20;
-  let connection;
-  return this.getConnection().then(conn => {
-    connection = conn;
-    return connection.query('SET SESSION wait_timeout = ?', [timeout]);
-  }).then(()=> {
-    return connection.query('START TRANSACTION');
-  }).then(() => {
-    return connection;
-  });
-};
-
-/**
- * Rollback the current transaction
- * @param  {Object} connection The connection object from startTransaction
- */
-DB.prototype.rollback = function (connection) {
-  if(connection){
-      return connection.execute('ROLLBACK').then(()=> {
-        connection.release();
-      }).catch(err => {
-        connection.release();
-
-        throw err;
-      });
-  }
-};
-
-/**
- * Commit the current transaction
- * @param  {Object} connection The connection object from startTransaction
- */
-DB.prototype.commit = function (connection) {
-  return connection.execute('COMMIT').then(()=> {
-    connection.release();
-  }).catch(err => {
-    connection.release();
-
-    throw err;
-  });
-};
-
-module.exports = function (opts) {
-  if (!opts || Object.keys(opts).length <= 0) {
-    throw new Error('The config object cannot be empty');
-  }
-
-  let config = JSON.parse(JSON.stringify(opts));
-  let name = '_default_';
-
-  if (config.name) {
-    name = config.name;
-    delete config.name;
-  }
-
-  if (!instances[name]) {
-    let instance = new DB();
-    instance.configure(config);
-    instances[name] = instance;
-  }
-
-  return instances[name];
-};
+module.exports = init;
